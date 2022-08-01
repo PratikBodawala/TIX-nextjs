43f485c04ca93b6d78e5f1f1bfad03b87014491f
Pratik Bodawala <pratik@propelius.tech>
2022-08-01 20:04:00 +0530

    :sparkles:font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,21 +9,37 @@ module.exports = {
         extend: {
           colors: {
             clifford: '#da373d',
-            btnPrimary: '#1A2C50',
-            textPrimary: '#FFBE00',
-            errorText: '#FF6B6B',
-            link: '#118EEA',
+            royalBlue: '#1A2C50',
+            sunshineYellow: '#FFBE00',
+            sweetRed: '#FF6B6B',
+            skyBlue: '#118EEA',
             white:'#FFFFFF',
-            ticket: '#F2C46F',
-            redPrimary: '#EC1E2B',
+            pastelYellow: '#F2C46F',
+            cgvRed: '#EC1E2B',
             xxiGradient: '#',
             cinepolisBlue: '#000E62',
+
             btnDefault: '#1A2C50',
             btnHover: '#282764',
-            btnPressed: '#383782'
+            btnPressed: '#383782',
+
+            shade900:'#333333',
+            shade800:'#333D58',
+            shade700:'#414A62',
+            shade600:'#5A637A',
+            shade500:'#8F98AA',
+            shade400:'#9DA8BE',
+            shade300:'#BDC5D4',
+            shade200:'#DADFE8',
+            shade100:'#FFFFFF',
           },
           backgroundImage:{
             'theater': 'url("/images/theater-bg-img.jpeg")',
+          },
+          fontFamily: {
+            "body": ["Roboto",
+              ...defaultTheme.fontFamily.sans
+            ],
           }
         }
       },
