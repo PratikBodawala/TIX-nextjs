66428db7c924775a83f440ab44c13ef0e4c61184
Pratik Bodawala <pratik@propelius.tech>
2022-08-02 16:17:45 +0530

    :lipstick: login and create account

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -7,8 +7,8 @@ const Login = () => {
             <h2 className='font-bold text-4xl my-4'>TIX ID</h2>
             <form className="w-[423px] my-16">
                 <div className="mb-4">
-                    <label className="block text-shade900 text-lg font-normal mb-2" htmlFor="phone_number">
-                        PHONE NUMBER
+                    <label className="block text-shade900 text-lg font-normal mb-2 uppercase" htmlFor="phone_number">
+                        Phone number
                     </label>
                     <div className={styles.phoneInputBox}>
                         <span className="prefix">+91</span>
@@ -18,8 +18,8 @@ const Login = () => {
                     </div>
                 </div>
                 <div className="mb-6">
-                    <label className="block text-shade900 text-lg font-normal mb-2" htmlFor="password">
-                        PASSWORD
+                    <label className="block text-shade900 text-lg font-normal mb-2 uppercase" htmlFor="password">
+                        Password
                     </label>
                     <div className={styles.phoneInputBox}>
                     <input id="password" type="password" placeholder="enter password here"/>
@@ -27,16 +27,14 @@ const Login = () => {
                     </div>
                     {/*<p className="text-red-500 text-xs italic">Please choose a password.</p>*/}
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="flex flex-col items-center justify-between mt-16">
                     <button
-                        className="bg-royalBlue hover:bg-btnHover text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-royalBlue hover:bg-btnHover text-white text-xl font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full uppercase"
                         type="button">
-                        LOGIN
+                        Login
                     </button>
-                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                       href="#">
-                        Forgot Password?
-                    </a>
+                    <span className="text-xs mt-6 text-shade600">don&apos;t have account?</span>
+                    <button className="mt-2 font-default text-royalBlue rounded border-[1px] border-shade600 text-xl font-medium w-full h-12">Create account</button>
                 </div>
             </form>
 
